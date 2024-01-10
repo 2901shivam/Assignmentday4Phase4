fecd7d0f1be0364fbefaa7853c5b0bb04fe7967b
2901shivam <shivamsingh201617@gmail.com>
2024-01-10 15:03:38 +0530

    assignmentDayFour

---
+++ Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentDayFour
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string? PName { get; set; }
+
+        public int Price { get; set; }
+
+        public string? PBrand { get; set; }
+
+        public DateTime? ManufactureDate { get; set; }
+
+        public DateTime? ExpDate { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+using AssignmentDayFour;
+
+List<Product> product = new List<Product>()
+{
+    new Product(){Id=1,PName="AntiHarfall Shampoo",Price=150,PBrand="Head Shoulder",ManufactureDate=new DateTime(day:12,month:11,year:2022),ExpDate=new DateTime(day:10,month:02,year:2024)},
+    new Product(){Id=2,PName="GoodDay",Price=40,PBrand="Britania",ManufactureDate=new DateTime(day:12,month:11,year:2023),ExpDate=new DateTime(day:10,month:02,year:2025)},
+    new Product(){Id=3,PName="Glucose Biscut",Price=10,PBrand="Parle G",ManufactureDate=new DateTime(day:12,month:11,year:2022),ExpDate=new DateTime(day:10,month:02,year:2024)},
+    new Product(){Id=4,PName="Kit kat",Price=100,PBrand="Cadburry",ManufactureDate=new DateTime(day:12,month:11,year:2022),ExpDate=new DateTime(day:10,month:02,year:2024)},
+    new Product(){Id=5,PName="FiveStar",Price=50,PBrand="Cadburry",ManufactureDate=new DateTime(day:12,month:11,year:2022),ExpDate=new DateTime(day:10,month:02,year:2024)},
+
+
+};
+foreach (Product prd in product)
+{
+    Console.WriteLine("Id" + prd.Id);
+    Console.WriteLine("PName:" + prd.PName);
+    Console.WriteLine("Price:" + prd.Price);
+    Console.WriteLine("PBrand:" + prd.PBrand);
+    Console.WriteLine("Manufacture" + prd.ManufactureDate);
+    Console.WriteLine("Exp:" + prd.ExpDate);
+}
